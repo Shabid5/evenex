7615dc3e808df7d1948cccd6be8c66f3981dee3f
Shabid5 <87390087+Shabid5@users.noreply.github.com>
2022-04-03 04:34:26 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$(function(){
+    $(function(){
+        $('.count-num').rCounter({
+          duration: 30
+        });
+      });
+    $(function () {
+        loopcounter('myCountdown');
+    });
+    
+});
